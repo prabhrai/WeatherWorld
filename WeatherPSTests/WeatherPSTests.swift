73e5f883702ba16caf866fba684e74a8228235c3
PS <PS@pss-mac.local>
2017-05-07 20:56:51 -0400

    Final App for project. Without core data

--- WeatherPSTests/WeatherPSTests.swift
+++ WeatherPSTests/WeatherPSTests.swift
@@ -10,9 +10,11 @@ import XCTest
 @testable import WeatherPS
 
 class WeatherPSTests: XCTestCase {
+
     
     override func setUp() {
         super.setUp()
+        
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
@@ -25,6 +27,7 @@ class WeatherPSTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
+
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
